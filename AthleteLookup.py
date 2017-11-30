b38cd2d0d9a3dd5c5ed4f206d75dff58b160264d
Reiss Jarvis <Reiss@Reisss-Mac-mini.local>
2017-11-30 21:51:49 +0000

    New function to look up an athlete.

---
+++ AthleteLookup.py
@@ -0,0 +1,40 @@
+# 1. Extract results for event from powerOfTen
+# 2. loop through results calculate running total
+# 3. Calculate and print Average
+
+import requests
+from bs4 import BeautifulSoup
+
+def fetchresults():
+    page1 = requests.get(poweroften)
+    soup1 = BeautifulSoup(page1.text, 'html.parser')
+    vstate = soup1.find(id='__VIEWSTATE')['value']
+    eventvali = soup1.find(id='__EVENTVALIDATION')['value']
+    payload = {'__VIEWSTATE': vstate,
+    '__EVENTVALIDATION': eventvali,
+    'ctl00$cphBody$txtSurname': 'Blake',
+    'ctl00$cphBody$txtFirstName': 'Lana',
+    'ctl00$cphBody$ctl00$cphBody$txtClub': 'Southampton',
+    'ctl00$cphBody$btnLookup': 'Lookup'}
+    #print(payload)
+    page2 = requests.post(poweroften, data=payload)
+    print(page2.status_code)
+    soup2 = BeautifulSoup(page2.text, 'html.parser')
+    searchresults = soup2.find(id='cphBody_dgAthletes') #ID in table of results
+    #print(searchresults)
+    if searchresults is None:
+        athletepage = None
+    else:
+        athletepage = searchresults.find_all('tr')[1].contents[8].a['href']
+    #print(type(performances))
+    #resultTags = performances.find_all(isresult)
+    #results = []
+    #for resultTag in resultTags:
+    #    if isevent(resultTag, event):
+    #        result = getresult(resultTag)
+    #        results.append(result)
+    return athletepage
+
+poweroften = "http://thepowerof10.info/athletes/athleteslookup.aspx"
+results = fetchresults()
+print(results)
