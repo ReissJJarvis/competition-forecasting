cc6897628324b4e6ffe421a965e89fc9f8fac874
Reiss Jarvis <Reiss@Reisss-Mac-mini.local>
2018-01-21 13:51:08 +0000

    Designed form and results page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -33,3 +35,15 @@ def login():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route('/forecast', methods=['GET', 'POST'])
+def forecast():
+    if request.method == 'POST':
+        app.logger.debug('request.form.event %s', request.form.get('event'))
+        app.logger.debug('request.form.fn %s', request.form.getlist('fn[]'))
+        app.logger.debug('request.form.ln %s', request.form.getlist('ln[]'))
+        app.logger.debug('request.form.cl %s', request.form.getlist('cl[]'))
+        #do_the_login()
+        return render_template('results.html', athletes=8)
+    else:
+        return render_template('forecastform.html', athletes=8)
